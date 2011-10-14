36c20e86ae612c3ef748ad1c877635b242d01eb4
Yury Bartashevich <y.bartashevich@bylectric.com>
2011-10-14 22:35:34 +0300

    Change books template file

--- sites/all/themes/fc_arsenal_theme/templates/views/views-view-fields--books--page.tpl.php
+++ sites/all/themes/fc_arsenal_theme/templates/views/views-view-fields--books--page.tpl.php
@@ -9,7 +9,7 @@
     <?php 
       $file = file_load($fields['field_book_file']->content);
       $pathinfo = pathinfo($file->uri);?>
-       Формат файла: <i><?php echo strtolower($pathinfo['extension']);?></i><br />
+       <?php echo t('File format')?>: <i><?php echo strtolower($pathinfo['extension']);?></i><br />
     <?php echo l(theme('image',array('path'=>drupal_get_path('theme','fc_arsenal_theme').'/img/download-icon-small.png','width'=>'16px')).' '.t('Download book') , 'file_download/'.$fields['field_book_file']->content,array('html'=>TRUE, 'attributes'=>array('title'=>t('Download file').' "'.$fields['title']->content.'"'))) ?>
   </div>
 </div>
