9870b8fda6d95024ea8d0f87a0a7a8f6976fda68
Yury Bartashevich <y.bartashevich@bylectric.com>
2011-10-21 02:39:27 +0300

    Add avatar to commets

--- sites/all/themes/fc_arsenal_theme/templates/general/comment.tpl.php
+++ sites/all/themes/fc_arsenal_theme/templates/general/comment.tpl.php
@@ -1,15 +1,17 @@
+
 <div class="comment <?php print ($new) ? ' comment-new' : ''; print ($status); ?> clear-block comment-<?php print $zebra; ?> ">
-	<div class="left-part">
-		<div class="comment-author"><?php print $author;?></div>
-	</div>
+  <div class="comment_user_picture">
+<?php print $picture;?>
+</div>
 	<div class="right-part">
+    <div class="comment-author"><?php print $author;?> <span class="comment-date">(<?php print render($content['posted_date']);?>)</span></div>
     <?php hide($content['links']) ?>
 		<?php print render($content); ?>
 	</div>
 	<div class="cf">&nbsp;</div>
 	<div class="comment-footer">
 		<div class="comment-link"><?php print render($content['links']); ?></div>
-		<div class="comment-date"><?php echo t('Date')?>:	<span class="info-value"><?php print render($content['posted_date']);?></span></div>
 	</div>
 	<div class="cf">&nbsp;</div>
 </div>
+<div class="cf">&nbsp;</div> 
