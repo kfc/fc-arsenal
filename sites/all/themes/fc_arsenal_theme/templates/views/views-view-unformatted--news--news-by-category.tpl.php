90ed32d9da77004fb32ef630af529ea8dfadd5cb
Yury Bartashevich <y.bartashevich@bylectric.com>
2012-02-14 00:45:31 +0300

    Changes

--- sites/all/themes/fc_arsenal_theme/templates/views/views-view-unformatted--news--news-by-category.tpl.php
+++ sites/all/themes/fc_arsenal_theme/templates/views/views-view-unformatted--news--news-by-category.tpl.php
@@ -1,6 +1,3 @@
-<?php if($variables['page_title']):?>
-  <h2><?php echo $variables['page_title'];?></h2>
-<?endif;?>
 <div class="news-block-content">
   <div>
     <?php foreach ($rows as $id => $row): ?>
