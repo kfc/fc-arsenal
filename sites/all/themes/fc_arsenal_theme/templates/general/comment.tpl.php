8a05567eceba014546bbe21434237dd5c34e25b9
Charlie Root <root@fc-arsenal.com>
2012-03-13 18:39:21 +0000

    HTML changes

--- sites/all/themes/fc_arsenal_theme/templates/general/comment.tpl.php
+++ sites/all/themes/fc_arsenal_theme/templates/general/comment.tpl.php
@@ -1,6 +1,5 @@
 <div class="comment <?php print ($new) ? ' comment-new' : ''; print ($status); ?> clear-block comment-<?php print $zebra;?> ">
-	<!--div class="comment_user_picture"><?php print $picture;?></div-->
-  <?php print $picture;?>
+	<div class="comment_user_picture"><?php print $picture;?></div>
 	<div class="comment-date date"><?php print render($content['posted_date']);?></div>
 	<div class="comment-author"><?php print $author;?></div>
 	<div class="cf">&nbsp;</div>
