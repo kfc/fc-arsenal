54afd0c3098496c9de2feb867014fc423fd6ccf7
Yury Bartashevich <y.bartashevich@bylectric.com>
2012-04-03 00:54:34 +0300

    Change hardcoded banner

--- sites/all/themes/fc_arsenal_theme/templates/views/views-view-unformatted--news--block-1.tpl.php
+++ sites/all/themes/fc_arsenal_theme/templates/views/views-view-unformatted--news--block-1.tpl.php
@@ -14,7 +14,7 @@
     ?>
     <!-- Hardcoded in view template views-view-unformatted--news--block-1.tpl.php  -->
     <script type="text/javascript" src="/sites/all/themes/fc_arsenal_theme/js/swfobject.js"></script>
-    <div id="soccermart"></div>
+    <div id="soccermart" style="margin-top: 10px;"></div>
     <script type="text/javascript">
      var params = {bgcolor:"#сс0000",  allowScriptAccess:"always", wmode:"opaque"};
      swfobject.embedSWF("/sites/default/files/arsenal_shirts.swf", "soccermart", "500", "100",
