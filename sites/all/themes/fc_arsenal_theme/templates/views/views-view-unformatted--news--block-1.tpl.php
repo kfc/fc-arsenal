e1db57798528b7d21f05ab625b6df0efa0c2e034
Yury Bartashevich <y.bartashevich@bylectric.com>
2012-04-02 17:17:15 +0300

    Add hardcoded banner to news wiew at homepage

--- sites/all/themes/fc_arsenal_theme/templates/views/views-view-unformatted--news--block-1.tpl.php
+++ sites/all/themes/fc_arsenal_theme/templates/views/views-view-unformatted--news--block-1.tpl.php
@@ -12,6 +12,9 @@
     <?php else:
       if($index == $variables['news_teaser_count']):
     ?>
+    <!-- Hardcoded in view template views-view-unformatted--news--block-1.tpl.php  -->
+    <div id="flashContent" style="margin-top: 10px; text-align: center;"><object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" id="500x100" width="500" align="middle" height="100"><param name="movie" value="500x100.swf"><param name="quality" value="high"><param name="bgcolor" value="#ffffff"><param name="play" value="true"><param name="loop" value="true"><param name="wmode" value="window"><param name="scale" value="showall"><param name="menu" value="true"><param name="devicefont" value="false"><param name="salign" value=""><param name="allowScriptAccess" value="sameDomain"><!--[if !IE]>--><object data="http://www.soccermart.ru/promo/arsenal/500x100.swf" type="application/x-shockwave-flash" width="500" height="100"><param name="movie" value="500x100.swf"><param name="quality" value="high"><param name="bgcolor" value="#ffffff"><param name="play" value="true"><param name="loop" value="true"><param name="wmode" value="window"><param name="scale" value="showall"><param name="menu" value="true"><param name="devicefont" value="false"><param name="salign" value=""><param name="allowScriptAccess" value="sameDomain"><!--<![endif]--><a href="http://www.adobe.com/go/getflash"> <img alt="Get Adobe Flash player" src="http://www.adobe.com/images/shared/download_buttons/get_flash_player.gif"> </a> <!--[if !IE]>--></object><!--<![endif]--></object></div>
+    <!-- End of hardcoded banner -->
     <div class="item-list">                                           
       <ul>
      <?endif;?> 
